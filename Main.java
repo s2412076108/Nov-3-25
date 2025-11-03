0141de72f955fb99d60dfcc57b33c0b86c56e538
Nirnita Banik <s2412076108@ru.ac.bd>
2025-11-03 21:10:16 +0600

    Main.java code commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import first_package.My_Package;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        My_Package ob = new My_Package();
+        ob.addition();
+    }
+}
+
